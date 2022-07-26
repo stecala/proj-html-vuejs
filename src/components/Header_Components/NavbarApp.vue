34e315e186781db0a2ed80207bb9fe52a8f7173d
stecala <ste.cala123@gmail.com>
2022-07-26 13:34:07 +0200

    Feat (create Array) ⭐ Create array for navbar

--- src/components/Header_Components/NavbarApp.vue
+++ src/components/Header_Components/NavbarApp.vue
@@ -8,7 +8,56 @@
 export default {
     data : function (){
         return{
-        
+            nav : [
+                {
+                    name : 'Home',
+                    url : '#',
+                    new : false,
+                    selected : true,
+                },
+                {
+                    name : 'Cullinary History',
+                    url : '#',
+                    new : false,
+                    selected : false,
+                },
+                {
+                    name : 'Our Team',
+                    url : '#',
+                    new : false,
+                    selected : false,
+                },
+                {
+                    name : 'Our Menu',
+                    url : '#',
+                    new : false,
+                    selected : false,
+                },
+                {
+                    name : 'Takeout',
+                    url : '#',
+                    new : true,
+                    selected : false,
+                },
+                {
+                    name : 'Bulletin',
+                    url : '#',
+                    new : false,
+                    selected : false,
+                },
+                {
+                    name : 'Reservation',
+                    url : '#',
+                    new : false,
+                    selected : false,
+                },
+                {
+                    name : '<i class="fa-solid fa-cart-shopping"></i>',
+                    url : '#',
+                    new : false,
+                    selected : false,
+                },
+            ],
         
         }
     },
