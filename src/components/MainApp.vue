cdcb739a081ce9981f095b9975237f340de80659
stecala <ste.cala123@gmail.com>
2022-07-26 19:21:12 +0200

    Feat (create component) ⭐ create and imported new component PromoList in main

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,14 +1,17 @@
 <template>
   <main class="mt-5">
       <PromoComponent />
+      <PromoList />
   </main>
 </template>
 
 <script>
 import PromoComponent from './Main_Components/PromoComponent.vue'
+import PromoList from './Main_Components/PromoList.vue'
 export default {
   components :{
     PromoComponent,
+    PromoList,
   }
 }
 </script>
