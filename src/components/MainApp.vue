90e7112a87b2cd6c1ac69ece1b81f61fb2e6f3d7
stecala <ste.cala123@gmail.com>
2022-07-26 20:43:11 +0200

    Feat (create array) ⭐ create array for PromoCardsList

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,18 +1,48 @@
 <template>
   <main class="mt-5">
       <PromoComponent />
-      <PromoList />
+      <PromoCardsList :cardsList="cardsList" />
   </main>
 </template>
 
 <script>
 import PromoComponent from './Main_Components/PromoComponent.vue'
-import PromoList from './Main_Components/PromoList.vue'
+import PromoCardsList from './Main_Components/PromoCardsList.vue'
 export default {
   components :{
     PromoComponent,
-    PromoList,
-  }
+    PromoCardsList,
+  }, 
+  data : function(){
+    return{
+      cardsList : [
+        {
+          img : '../../../public/img/sushi-1.png',
+          txt : 'Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.',
+          btn : {
+            url : '#',
+            'txt_btn': 'explore the menu',
+          }
+        },
+         {
+          img : '../../../public/img/sushi-2.png',
+          txt : 'Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.',
+          btn : {
+            url : '#',
+            'txt_btn': 'make a reservation',
+          }
+        },
+         {
+          img : '../../../public/img/sushi-3.png',
+          txt : 'Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.',
+          btn : {
+            url : '#',
+            'txt_btn': 'learn more about us',
+          }
+        },
+      ]
+    }
+  },
 }
 </script>
 
--- src/components/Main_Components/PromoCardsList.vue
+++ src/components/Main_Components/PromoCardsList.vue
@@ -6,7 +6,9 @@
 
 <script>
 export default {
-
+  props: {
+    cardsList : Array,
+  }
 }
 </script>
 
