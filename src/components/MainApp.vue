32b59faa7027f874ce03504ce9ad9a3cc9128e7e
stecala <ste.cala123@gmail.com>
2022-07-26 18:23:43 +0200

    Chore 🪛 Import PromoComponent to MainApp

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,12 +1,15 @@
 <template>
   <main class="mt-5">
-    
+      <PromoComponent />
   </main>
 </template>
 
 <script>
+import PromoComponent from './Main_Components/PromoComponent.vue'
 export default {
-
+  components :{
+    PromoComponent,
+  }
 }
 </script>
 
