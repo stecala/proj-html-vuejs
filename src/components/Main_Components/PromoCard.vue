83eeadf693c8efb8f32dd0c1784d0517071ca3b7
stecala <ste.cala123@gmail.com>
2022-07-26 20:48:40 +0200

    Feat (create component) ⭐ create component PromoCard and imported

---
+++ src/components/Main_Components/PromoCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-4">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Main_Components/PromoCardsList.vue
+++ src/components/Main_Components/PromoCardsList.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    
+  <div class="container-lg mt-5">
+     <div class="row">
+        <PromoCard />
+     </div>
   </div>
 </template>
 
 <script>
+import PromoCard from './PromoCard.vue'
 export default {
+  components :{
+    PromoCard,
+  },
   props: {
     cardsList : Array,
   }
