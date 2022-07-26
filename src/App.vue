4e49c8c5d9f51df716881feb22d3a60e948e0f46
stecala <ste.cala123@gmail.com>
2022-07-26 12:27:16 +0200

    Chore 🪛 import of components in app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
       <HeaderApp />
+      <MainApp />
+      <FooterApp />
   </div>
 </template>
 
 <script>
 import HeaderApp from './components/HeaderApp.vue'
+import MainApp from './components/MainApp.vue'
+import FooterApp from './components/FooterApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderApp
+    HeaderApp,
+    MainApp,
+    FooterApp,
   }
 }
 </script>
