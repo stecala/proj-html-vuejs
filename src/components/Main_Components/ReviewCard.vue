b8b6a8d409d117cb2289b7c41c92fd53417e4782
stecala <ste.cala123@gmail.com>
2022-07-27 13:02:42 +0200

    Feat (end component) ⭐ ReviewCard is ready

--- src/components/Main_Components/ReviewCard.vue
+++ src/components/Main_Components/ReviewCard.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-
+  <div class="review-container">
+    <div class="reviewer text-uppercase">{{element.reviewer}}</div>
+    <div class="journal mt-4 text-uppercase">{{element.journal}}</div>
+    <div v-for=" number in element.stars" :key="number" class="d-inline-block mt-4 me-2"><i class="fa-solid fa-star"></i></div>
+    <div class="line mt-4"></div>
+    <div class="txt mt-4">{{element.txt.paragraph1}}<br>
+        <div class="spacer"></div>
+        <span v-if="(element.txt.paragraph2 !='')">{{element.txt.paragraph2}}</span>
+    </div>
+      <div class="my-btn-white mt-5">
+        <a :href="element.btn.url">{{element.btn.text}}</a>
+      </div>
   </div>
 </template>
 
@@ -12,6 +22,34 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../assets/style/variables.scss';
 
+    .review-container{
+        padding: 0 120px;   
+        color: $txtColorWhite;
+        font-family: 'Bebas Neue', cursive;
+        .reviewer{
+            font-size: 1rem;
+            font-family: 'Poppins', sans-serif;
+        }
+        .journal{
+            font-size: 3.5rem;
+            line-height: 1.2;
+        }
+        .line{
+            width: 70px;
+            height: 1px;
+            background-color: $txtColorUnselected;
+        }
+        .txt{
+            padding-right: 20px;
+            font-size: 1.2rem;
+            font-family: 'Ibarra Real Nova', serif;
+            .spacer{
+                width: 100%;
+                height: 10px;
+            }
+        } 
+    }
 </style>
--- src/components/Main_Components/ReviewSection.vue
+++ src/components/Main_Components/ReviewSection.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container-lg mt-5 pt-5">
-    <div class="col-6 img-container">
-        <img src="/img/blog72x-1200x1375.jpg" alt="Girl cooking">
-    </div>
-    <div class="col-6 text-zone align-items center">
-        <ReviewCard :element="textCardsReview.firstReview" />
+  <div class="container-lg mt-5 pt-3">
+    <div class="row">
+      <div class="col-6 img-container">
+          <img src="/img/blog72x-1200x1375.jpg" alt="Girl cooking">
+      </div>
+      <div class="col-6 text-zone d-flex align-items-center card-container justify-content-center">
+          <ReviewCard :element="textCardsReview.firstReview" />
+      </div>
     </div>
   </div>
 </template>
@@ -20,9 +22,12 @@ export default {
       textCardsReview: {
         firstReview :{
           reviewer : 'meggy stewart',
-          Journal : 'new york times',
-          stars :  '5',
-          txt : 'Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt, id sapien accumsai, sed at. Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi.',
+          journal : 'new york times',
+          stars :  5,
+          txt : {
+            paragraph1 : 'Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt, id sapien accumsai, sed at.' ,
+            paragraph2 : 'Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi.'
+          },
           btn : {
             url : '#',
             text : 'read full article'
@@ -30,9 +35,12 @@ export default {
         },
         secondReview :{
           reviewer : 'mary maxey',
-          Journal : 'the guardian',
+          journal : 'the guardian',
           stars :  '5',
-          txt : 'Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed.',
+          txt : {
+            paragraph1 : 'Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed.',
+            paragraph2 : ''
+            },
           btn : {
             url : '#',
             text : 'read full article'
@@ -40,9 +48,12 @@ export default {
         },
         thirdReview :{
           reviewer : 'patrick monroe',
-          Journal : 'globe and mail',
+          journal : 'globe and mail',
           stars :  '5',
-          txt : 'Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt.',
+          txt : {
+            paragraph1 : 'Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt.',
+            paragraph2 : ''
+            },
           btn : {
             url : '#',
             text : 'read full article'
@@ -57,10 +68,18 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../../assets/style/variables.scss';
+    .col-6{
+      padding: 0;
+    }
     .img-container{
         height: 700px;
         img{
             width: 100%;
+            height: 100%;
         }
+    }
+    .card-container{
+      background-color: $blackBg;
     }
 </style>
