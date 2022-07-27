418ba49bda68921894b49c143b9b0962477205eb
stecala <ste.cala123@gmail.com>
2022-07-27 12:04:23 +0200

    Feat (create Object) ⭐ Create object with info for ReviewCard

--- src/components/Main_Components/ReviewCard.vue
+++ src/components/Main_Components/ReviewCard.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    
+
   </div>
 </template>
 
 <script>
 export default {
-
+    props : {
+        element : Object,
+    }
 }
 </script>
 
--- src/components/Main_Components/ReviewSection.vue
+++ src/components/Main_Components/ReviewSection.vue
@@ -3,14 +3,56 @@
     <div class="col-6 img-container">
         <img src="/img/blog72x-1200x1375.jpg" alt="Girl cooking">
     </div>
-    <div class="col-6 text-zone"></div>
-    
+    <div class="col-6 text-zone align-items center">
+        <ReviewCard :element="textCardsReview.firstReview" />
+    </div>
   </div>
 </template>
 
 <script>
+import ReviewCard from './ReviewCard.vue'
 export default {
-
+  components : {
+    ReviewCard,
+  },
+  data : function(){
+    return {
+      textCardsReview: {
+        firstReview :{
+          reviewer : 'meggy stewart',
+          Journal : 'new york times',
+          stars :  '5',
+          txt : 'Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt, id sapien accumsai, sed at. Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi.',
+          btn : {
+            url : '#',
+            text : 'read full article'
+          } 
+        },
+        secondReview :{
+          reviewer : 'mary maxey',
+          Journal : 'the guardian',
+          stars :  '5',
+          txt : 'Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed.',
+          btn : {
+            url : '#',
+            text : 'read full article'
+          } 
+        },
+        thirdReview :{
+          reviewer : 'patrick monroe',
+          Journal : 'globe and mail',
+          stars :  '5',
+          txt : 'Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt.',
+          btn : {
+            url : '#',
+            text : 'read full article'
+          } 
+        },
+      
+      
+      }
+    }
+  }
 }
 </script>
 
