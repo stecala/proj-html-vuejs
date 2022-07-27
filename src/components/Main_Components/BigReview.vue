1f49972611fb83a071c94879b191b2e6e8b0409d
stecala <ste.cala123@gmail.com>
2022-07-27 11:19:02 +0200

    Feat (create component) ⭐ create Review Card

--- src/components/Main_Components/BigReview.vue
+++ src/components/Main_Components/BigReview.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div class="container-lg mt-5 pt-5">
+    <div class="col-6 img-container">
+        <img src="/img/blog72x-1200x1375.jpg" alt="Girl cooking">
+    </div>
+    <div class="col-6"></div>
     
   </div>
 </template>
@@ -10,6 +14,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+    .img-container{
+        height: 700px;
+        img{
+            width: 100%;
+        }
+    }
 </style>
